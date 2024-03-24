4f034c416306fa266a88130864b7b494aa36d665
Spark Knight <1120939526@qq.com>
2024-03-24 11:37:33 +0800

    init 3 packages, with java as the static proxy

---
+++ src/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BeProxyed bp = new BeProxyed();
+        Proxy proxy = new Proxy(bp);
+        proxy.proxySayHello("World");
+    }
+}
+
+class Proxy {
+    public Consumer consumer;
+
+    public Proxy(Consumer consumer) {
+        this.consumer = consumer;
+    }
+
+    public void proxySayHello(String msg) {
+        consumer.sayHello(msg);
+    }
+}
+
+interface Consumer {
+    public void sayHello(String msg);
+}
+
+class BeProxyed implements Consumer {
+    public void sayHello(String msg) {
+        System.out.println("Hello, " + msg);
+    }
+}
